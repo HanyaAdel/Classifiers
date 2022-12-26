6f1ff6d1e8f66b3e5f55d55bb0b52d1ab94059d8
haneen-27 <97315001+haneen-27@users.noreply.github.com>
2022-12-27 01:28:18 +0200

    bayes updated

--- bayes.py
+++ bayes.py
@@ -6,62 +6,42 @@ class BayesClassifier:
     def __init__(self, legalLabels):
         self.legalLabels = legalLabels
         self.type = "naivebayes"
-        self.k = 1 # this is the smoothing parameter, ** use it in your train method **
-        self.automaticTuning = True # Look at this flag to decide whether to choose k automatically ** use this in your train method **
+        self.alpha = 1    # this is the default laplacian smoothing parameter
+        self.automaticTuning = False     #Flag to decide whether to choose alpha automatically 
         self.probs = {}
         self.extra = False
 
-    def setSmoothing(self, k):
-        """
-        This is used by the main method to change the smoothing parameter before training.
-        Do not modify this method.
-        """
-        self.k = k
+    def setSmoothing(self, alpha):
+        self.alpha = alpha
 
     def train(self, trainingData, trainingLabels, validationData, validationLabels):
-        """
-        Outside shell to call your method. Do not modify this method.
-        """
 
-        # might be useful in your code later...
-        # this is a list of all features in the training set.
         self.features = list(set([ f for datum in trainingData for f in datum.keys() ]));
 
         if (self.automaticTuning):
-            kgrid = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
+            alpha_vals = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
         else:
-            kgrid = [self.k]
+            alpha_vals = [self.alpha]
 
-        self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, kgrid)
+        self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, alpha_vals)
 
-    def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, kgrid):
-        """
-        Trains the classifier by collecting counts over the training data, and
-        stores the Laplace smoothed estimates so that they can be used to classify.
-        Evaluate each value of k in kgrid to choose the smoothing parameter
-        that gives the best accuracy on the held-out validationData.
-        trainingData and validationData are lists of feature Counters.  The corresponding
-        label lists contain the correct label for each datum.
-        To get the list of all possible features or labels, use self.features and
-        self.legalLabels.
-        """
-
+    def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, alpha_vals):
+       
         count = util.Counter()
-        priorD = util.Counter()
+        prior = util.Counter()
 
         for label in trainingLabels:
             count[label] += 1
-            priorD[label] += 1
-
+            prior[label] += 1
 
         count = {}
 
-        for feat in self.features:
+        for features in self.features:
 
-            count[feat] = {}
+            count[features] = {}
 
             for label in self.legalLabels:
-                count[feat][label] = {
+                count[features][label] = {
                     0: 0,
                     1: 0
                 }
@@ -71,71 +51,82 @@ class BayesClassifier:
             datum = trainingData[i]
             label = trainingLabels[i]
 
-            for (feat, val) in datum.items():
-                count[feat][label][val] += 1
-
-        #print "priorD normalize" + str(priorD.normalize())
-
-        priorD.normalize()
-        self.priorD = priorD
+            for (features, val) in datum.items():
+                count[features][label][val] += 1
 
 
-        # Using Laplace smoothing to tune the data and find the best k value that gives the highest accuracy
-        bestK = -1
-        bestAcc = -1
+        prior.normalize()
+        self.prior = prior
+
+        prior[0] = 0.9
+        prior[1] = 0.1
+        print(prior)
+        '''
+        prior[0] = 0.07
+        prior[1] = 0.02
+        prior[2] = 0.03
+        prior[3] = 0.08
+        prior[4] = 0.05
+        prior[5] = 0.04
+        prior[6] = 0.05
+        prior[7] = 0.06
+        prior[8] = 0.01
+        prior[9] = 0.09
+        '''
+
+        # Using Laplace smoothing to tune the data and find the best alpha value that gives the highest accuracy
+        bestAlpha = -1
+        bestAccuracy = -1
         # laplace smoothing equation 
-        for k in kgrid:
+        for alpha in alpha_vals:
             tempProb = {}
-            for (feat, labels) in count.items():
-                tempProb[feat] = {}
+            for (features, labels) in count.items():
+                tempProb[features] = {}
                 for (label, vals) in labels.items():
-                    tempProb[feat][label] = {}
-                    total = sum(count[feat][label].values())
-                    total += 2*k
+                    tempProb[features][label] = {}
+                    total = sum(count[features][label].values())
+                    total += 2*alpha
                     for (val, c) in vals.items():
                         #Normalizing the probability
-                        tempProb[feat][label][val] = (count[feat][label][val] + k) / total
+                        tempProb[features][label][val] = (count[features][label][val] + alpha) / total
 
             self.probs = tempProb
 
             predictions = self.classify(validationData)
 
             # Count number of correct predictions
-            acc = 0
+            accuracy = 0
             for i in range(len(predictions)):
                 if predictions[i] == validationLabels[i]:
-                    acc += 1
+                    accuracy += 1
 
-            # Checking if any of the k values produced the best accuracy and if it did we store it
-            if acc > bestAcc:
-                bestK = k
-                bestAcc = acc
+            # Checking if any of the alpha values produced the best accuracy and if it did we store it
+            if accuracy > bestAccuracy:
+                bestAlpha = alpha
+                bestAccuracy = accuracy
         
-        print(bestK)
-        print(bestAcc)
+        print(bestAlpha)
+        print(bestAccuracy)
         
 
-        #Calculating the probabilities using the best k to get the most accurate results
+        #Calculating the probabilities using the best alpha to get the most accurate results
         tProb = {}
-        for (feat, labels) in count.items():
-            tProb[feat] = {}
+        for (features, labels) in count.items():
+            tProb[features] = {}
 
             for (label, vals) in labels.items():
-                tProb[feat][label] = {}
-                total = sum(count[feat][label].values())
-                total += 2*bestK
+                tProb[features][label] = {}
+                total = sum(count[features][label].values())
+                total += 2*bestAlpha
                 for (val, c) in vals.items():
-                    tProb[feat][label][val] = (count[feat][label][val] + bestK) / total
+                    tProb[features][label][val] = (count[features][label][val] + bestAlpha) / total
 
         self.probs = tProb
 
     def classify(self, testData):
-        """
-        Classify the data based on the posterior distribution over labels.
-        You shouldn't modify this method.
-        """
+
         guesses = []
-        self.posteriors = [] # Log posteriors are stored for later data analysis (autograder).
+        self.posteriors = [] 
         for datum in testData:
             posterior = self.calculateLogJointProbabilities(datum)
             guesses.append(posterior.argMax())
@@ -143,23 +134,13 @@ class BayesClassifier:
         return guesses
 
     def calculateLogJointProbabilities(self, datum):
-        """
-        Returns the log-joint distribution over legal labels and the datum.
-        Each log-probability should be stored in the log-joint counter, e.g.
-        logJoint[3] = <Estimate of log( P(Label = 3, datum) )>
-        To get the list of all possible features or labels, use self.features and
-        self.legalLabels.
-        """
         logJoint = util.Counter()
-        #print self.priorD
 
         for label in self.legalLabels:
-            logJoint[label] = math.log(self.priorD[label])
-            #print str(logJoint[label])
+            logJoint[label] = math.log(self.prior[label])
 
-            for (feat, val) in datum.items():
-                #print "inside of datum for loop"
-                p = self.probs[feat][label][val];
+            for (features, val) in datum.items():
+                p = self.probs[features][label][val];
                 logJoint[label] += math.log(p)
 
         return logJoint
