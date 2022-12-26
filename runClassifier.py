9e94a30a46bab84ba9f460e03bfecdd81e2f4bd7
HanyaAdel <hanya.elged@gmail.com>
2022-12-26 22:36:13 +0200

    progress in visualization

--- runClassifier.py
+++ runClassifier.py
@@ -1,14 +1,15 @@
 from knn import KnnClassifier
-from bayes import BayesClassifier
+from bayes import NaiveBayesClassifier
 import samples
 import util
+from matplotlib import pyplot as plt
 DIGIT_DATUM_WIDTH = 28
 DIGIT_DATUM_HEIGHT = 28
 FACE_DATUM_WIDTH = 60
 FACE_DATUM_HEIGHT = 70
 
 
-TRAINING_DATA_SIZE_DIGITS = 50
+TRAINING_DATA_SIZE_DIGITS = 5000
 TESTING_DATA_SIZE_DIGITS = 1000
 VALIDATION_DATA_SIZE_DIGITS = 1000
 
@@ -98,17 +99,10 @@ def runClassifier():
     digitLegalLabels = range(10)
     faceLegalLabels = range(2)
 
-    # classifier = KnnClassifier(digitLegalLabels,3, 0)
-    # classifier.train(digitTrainingData, digitTrainingLabels, digitValidationData, digitValidationLabels)
-    # guesses = classifier.classify(digitValidationData)
-    # correct = [guesses[i] == digitValidationLabels[i] for i in range(len(digitValidationLabels))].count(True)
-    # print (str(correct), ("correct out of " + str(len(digitValidationLabels)) + " (%.1f%%).") % (100.0 * correct / len(digitValidationLabels)))
-    
-    # classifier = KnnClassifier(digitLegalLabels,3, 1)
-    # classifier.train(digitTrainingData, digitTrainingLabels, digitValidationData, digitValidationLabels)
-    # guesses = classifier.classify(digitValidationData)
-    # correct = [guesses[i] == digitValidationLabels[i] for i in range(len(digitValidationLabels))].count(True)
-    # print (str(correct), ("correct out of " + str(len(digitValidationLabels)) + " (%.1f%%).") % (100.0 * correct / len(digitValidationLabels)))
+
+    runKNN(trainingData=digitTrainingData, validationData=digitValidationData, testingData=digitTestingData,
+    legalLabels=digitLegalLabels, trainingLabels=digitTrainingLabels, validationLabels=digitValidationLabels,
+    testingLabels=digitTestingLabels)
 
     # classifier = BayesClassifier(faceLegalLabels)
     # classifier.train(faceTrainingData, faceTrainingLabels, faceValidationData, rawFaceValidationLabels)
@@ -116,11 +110,72 @@ def runClassifier():
     # correct = [guesses[i] == rawFaceValidationLabels[i] for i in range(len(rawFaceValidationLabels))].count(True)
     # print (str(correct), ("correct out of " + str(len(rawFaceValidationLabels)) + " (%.1f%%).") % (100.0 * correct / len(rawFaceValidationLabels)))
 
-    classifier = BayesClassifier(digitLegalLabels)
+    classifier = NaiveBayesClassifier(digitLegalLabels)
     classifier.train(digitTrainingData, digitTrainingLabels, digitValidationData, digitValidationLabels)
     guesses = classifier.classify(digitValidationData)
     correct = [guesses[i] == digitValidationLabels[i] for i in range(len(digitValidationLabels))].count(True)
     print (str(correct), ("correct out of " + str(len(digitValidationLabels)) + " (%.1f%%).") % (100.0 * correct / len(digitValidationLabels)))
 
+
+
+def runKNN(trainingData, validationData, testingData, legalLabels, 
+trainingLabels, validationLabels, testingLabels):
+
+    plot1 = plt.subplot2grid((5, 5), (0,0))
+    plot2 = plt.subplot2grid((5, 5), (0,2))
+    Kstats  = []
+    for k in range(2, 7):
+        classifier = KnnClassifier(legalLabels,k, 0)
+        classifier.train(trainingData, trainingLabels, validationData, validationLabels)
+        print ("Validating...")
+        guesses = classifier.classify(validationData)
+        correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+        print("using a k of ", k, " and distance mentric ", 0)
+        print (str(correct), ("correct out of " + str(len(validationLabels)) + " (%.1f%%).") % (100.0 * correct / len(validationLabels)))
+        print ("Testing...")
+        guesses = classifier.classify(testingData)
+        correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
+        print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))
+        accuracy = 100.0 * correct / len(testingLabels)
+        Kstats.append((k,round(accuracy, 2)))
+    print(Kstats)
+    xs = [x[0] for x in Kstats]
+    ys = [x[1] for x in Kstats]
+    plot1.plot(xs, ys)
+    plot1.set_title("Accuracy change with the change of the number of neighbors")
+    plot1.set_xlabel("number of neighbors (k)")
+    plot1.set_ylabel("Accuracy")
+
+    distanceMetricStats = {"Euclidean" : 0, "Manhattan" : 0}
+    classifier = KnnClassifier(legalLabels,4, 0)
+    classifier.train(trainingData, trainingLabels, validationData, validationLabels)
+    guesses = classifier.classify(validationData)
+    #correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+    print ("Testing...")
+    guesses = classifier.classify(testingData)
+    correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))
+    accuracy = 100.0 * correct / len(testingLabels)
+    distanceMetricStats["Euclidean"] = round(accuracy, 3)
+
+    classifier = KnnClassifier(legalLabels,4, 1)
+    classifier.train(trainingData, trainingLabels, validationData, validationLabels)
+    guesses = classifier.classify(validationData)
+    #correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+    print ("Testing...")
+    guesses = classifier.classify(testingData)
+    correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))
+    accuracy = 100.0 * correct / len(testingLabels)
+    distanceMetricStats["Manhattan"] = round(accuracy, 3)    
+    plot2.bar(list(distanceMetricStats.keys()), list(distanceMetricStats.values()))
+
+    plot2.set_title("Accuracy of different distance metrics")
+    plot2.set_xlabel("Distance Metrics")
+    plot2.set_ylabel("Accuracy")
+
+    plt.title("KNN")
+    plt.tight_layout()
+    plt.show()
 if __name__ == '__main__':
     runClassifier()
