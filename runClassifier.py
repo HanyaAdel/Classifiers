64cfeb215e518852249658487834df461d964e44
haneen-27 <97315001+haneen-27@users.noreply.github.com>
2023-01-08 16:28:33 +0200

    added mlp and svm

--- runClassifier.py
+++ runClassifier.py
@@ -1,10 +1,11 @@
 from knn import KnnClassifier
 from bayes import BayesClassifier
+from perceptron import MLP
+from svm import SVMClassifier
 import samples
 import util
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.naive_bayes import GaussianNB
 
 DIGIT_DATUM_WIDTH = 28
 DIGIT_DATUM_HEIGHT = 28
@@ -122,16 +123,13 @@ def runClassifier():
     faceValidationLabels = np.array(faceValidationLabels)
     faceTestingLabels = np.array(faceTestingLabels)    
 
-    classifier = GaussianNB()
-    classifier.fit(faceTrainingData, faceTrainingLabels)
+    #runMLP(trainingData=digitTrainingData, validationData=digitValidationData, testingData=digitTestingData,
+    #trainingLabels=digitTrainingLabels, validationLabels=digitValidationLabels,
+    #testingLabels=digitTestingLabels)
 
-    guesses = []
-    for datum in faceTestingData:
-      guesses.append(classifier.predict([datum]))
-    
-    correct = [guesses[i] == faceTestingLabels[i] for i in range(len(faceTestingLabels))].count(True)
-    print (str(correct), ("correct out of " + str(len(faceTestingLabels)) + " (%.1f%%).") % (100.0 * correct / len(faceTestingLabels)))
-  
+    #runSVM(trainingData=digitTrainingData, validationData=digitValidationData, testingData=digitTestingData,
+    #trainingLabels=digitTrainingLabels, validationLabels=digitValidationLabels,
+    #testingLabels=digitTestingLabels)
 
     # runKNN(trainingData=digitTrainingData, validationData=digitValidationData, testingData=digitTestingData,
     # trainingLabels=digitTrainingLabels, validationLabels=digitValidationLabels,
@@ -145,30 +143,131 @@ def runClassifier():
     # trainingLabels=digitTrainingLabels, validationLabels=digitValidationLabels,
     # testingLabels=digitTestingLabels)
 
-    # runBayes(trainingData=faceTrainingData, validationData=faceValidationData, testingData=faceTestingData,
-    # trainingLabels=faceTrainingLabels, validationLabels=faceValidationLabels,
-    # testingLabels=faceTestingLabels)    
+    #runBayes(trainingData=faceTrainingData, validationData=faceValidationData, testingData=faceTestingData,
+    #trainingLabels=faceTrainingLabels, validationLabels=faceValidationLabels,
+    #testingLabels=faceTestingLabels)    
 
+    #runMLP(trainingData=faceTrainingData, validationData=faceValidationData, testingData=faceTestingData,
+    #trainingLabels=faceTrainingLabels, validationLabels=faceValidationLabels,
+    #testingLabels=faceTestingLabels)  
+
+    runSVM(trainingData=faceTrainingData, validationData=faceValidationData, testingData=faceTestingData,
+    trainingLabels=faceTrainingLabels, validationLabels=faceValidationLabels,
+    testingLabels=faceTestingLabels)  
+  
+
+def runSVM(trainingData, validationData, testingData, 
+trainingLabels, validationLabels, testingLabels):
+  
+  c_values = [0.01, 0.1, 1, 10, 100]
+  gamma_values= [0.0001, 0.001, 0.01, 0.1, 1, 10]
+
+  c_stats = []
+  gamma_stats = []
+
+  for i in range(len(c_values)):
+    classifier = SVMClassifier(c_values[i], 0.01)
+
+    print("C ", c_values[i])
+
+    print("Training....")
+    classifier.train(trainingData, trainingLabels)
+
+    print("Validating....")
+    guesses = classifier.classify(validationData)
+    correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(validationLabels)) + " (%.1f%%).") % (100.0 * correct / len(validationLabels)))
+    
+    '''print("Testing....")
+    guesses = classifier.classify(testingData)
+    correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))'''
+
+    accuracy = 100.0 * (correct / len(validationLabels))
+    c_stats.append((c_values[i],round(accuracy, 2)))
+  xs = [x[0] for x in c_stats]
+  ys = [x[1] for x in c_stats]
+  plt.xticks(c_values)
+  plt.plot(xs, ys)
+  #plt.grid(True)
+  plt.title("Accuracy change with the change of the C parameter, gamma = '10' ")
+  plt.xlabel("C parameter")
+  plt.ylabel("Accuracy")    
+  plt.show()
+
+
+def runMLP(trainingData, validationData, testingData, 
+trainingLabels, validationLabels, testingLabels):
+
+  learning = [0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5]
+  activation = ['identity', 'logistic', 'tanh', 'relu']
+
+  activation_stats = []
+  learning_stats = []
+
+  for i in range(len(learning)):
+    classifier = MLP( 'relu', learning[i])
+
+    print("Learning Rate ", learning[i])
+
+    print("Training....")
+    classifier.train(trainingData, trainingLabels)
+
+    print("Validating....")
+    guesses = classifier.classify(validationData)
+    correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(validationLabels)) + " (%.1f%%).") % (100.0 * correct / len(validationLabels)))
+    
+    '''print("Testing....")
+    guesses = classifier.classify(testingData)
+    correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))'''
+
+    accuracy = 100.0 * (correct / len(validationLabels))
+    learning_stats.append((learning[i],round(accuracy, 2)))
+  xs = [x[0] for x in learning_stats]
+  ys = [x[1] for x in learning_stats]
+  plt.plot(xs, ys)
+  plt.xticks(learning, labels=learning, rotation ='vertical')
+  plt.title("Accuracy change with the change of learning rate and activation = 'relu'")
+  plt.xlabel("Learning Rate")
+  plt.ylabel("Accuracy")    
+  plt.show()
 
 
 def runBayes(trainingData, validationData, testingData, 
 trainingLabels, validationLabels, testingLabels):
+  alpha_set = [0.0001, 0.001, 0.01, 0.1]
 
-  classifier = BayesClassifier()
+  alpha_stats = []
 
-  print("Training....")
-  classifier.train(trainingData, trainingLabels, validationData, validationLabels)
+  for i in range(len(alpha_set)):
+    classifier = BayesClassifier(alpha_set[i])
+    print("variance smoothing value: ", alpha_set[i])
 
-  print("Validating....")
-  guesses = classifier.classify(validationData)
-  correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
-  print (str(correct), ("correct out of " + str(len(validationLabels)) + " (%.1f%%).") % (100.0 * correct / len(validationLabels)))
-  
-  print("Testing....")
-  guesses = classifier.classify(testingData)
-  correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
-  print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))
-        
+    print("Training....")
+    classifier.train(trainingData, trainingLabels)
+
+    print("Validating....")
+    guesses = classifier.classify(validationData)
+    correct = [guesses[i] == validationLabels[i] for i in range(len(validationLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(validationLabels)) + " (%.1f%%).") % (100.0 * correct / len(validationLabels)))
+    
+    print("Testing....")
+    guesses = classifier.classify(testingData)
+    correct = [guesses[i] == testingLabels[i] for i in range(len(testingLabels))].count(True)
+    print (str(correct), ("correct out of " + str(len(testingLabels)) + " (%.1f%%).") % (100.0 * correct / len(testingLabels)))
+
+    accuracy = 100.0 * correct / len(testingLabels)
+    alpha_stats.append((alpha_set[i],round(accuracy, 2)))
+  xs = [x[0] for x in alpha_stats]
+  ys = [x[1] for x in alpha_stats]
+  plt.xticks(alpha_set)
+  plt.plot(xs, ys)
+  plt.title("Accuracy change with the change of variance smoothing")
+  plt.xlabel("variance smoothing value")
+  plt.ylabel("Accuracy")    
+  plt.show()
 
 
 def runKNN(trainingData, validationData, testingData, 
---
+++ svm.py
@@ -0,0 +1,21 @@
+
+import util
+import math
+from sklearn.svm import SVC
+
+
+class SVMClassifier:
+    def __init__(self, c, gamma):
+        self.type = "support vector machine"
+        self.c = c
+        self.gamma = gamma
+        self.classifier = SVC(C=c, gamma=gamma)
+
+    def train(self, trainingData, trainingLabels):
+        self.classifier.fit(trainingData, trainingLabels)
+
+    def classify(self, testData):
+        guesses = []
+        for datum in testData:
+            guesses.append(self.classifier.predict([datum]))
+        return guesses
