9bf41b8e6fb8d17b9319cad2098f75c48b5ef58b
haneen-27 <97315001+haneen-27@users.noreply.github.com>
2023-01-08 16:27:54 +0200

    finalized code

--- bayes.py
+++ bayes.py
@@ -1,125 +1,21 @@
 import util
 import math
+from sklearn.naive_bayes import GaussianNB
 
 
 class BayesClassifier:
-    def __init__(self):
+    def __init__(self, v_smoothing):
         self.type = "naivebayes"
-        self.alpha = 1    # this is the default laplacian smoothing parameter
-        self.automaticTuning = False     #Flag to decide whether to choose alpha automatically 
         self.probs = {}
         self.extra = False
+        self.classifier = GaussianNB(var_smoothing = v_smoothing)
 
-    def setSmoothing(self, alpha):
-        self.alpha = alpha
-
-    def train(self, trainingData, trainingLabels, validationData, validationLabels):
-
-        self.features = list(set([ f for datum in trainingData for f in datum.keys() ]));
-
-        if (self.automaticTuning):
-            alpha_vals = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
-        else:
-            alpha_vals = [self.alpha]
-
-        self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, alpha_vals)
-
-    def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, alpha_vals):
-       
-        count = util.Counter()
-        prior = util.Counter()
-
-        for label in trainingLabels:
-            count[label] += 1
-            prior[label] += 1
-
-        count = {}
-
-        for features in self.features:
-
-            count[features] = {}
-
-            for label in self.legalLabels:
-                count[features][label] = {
-                    0: 0,
-                    1: 0
-                }
-
-        for i in range(len(trainingData)):
-
-            datum = trainingData[i]
-            label = trainingLabels[i]
-
-            for (features, val) in datum.items():
-                count[features][label][val] += 1
-
-
-        prior.normalize()
-        self.prior = prior
-
-        # Using Laplace smoothing to tune the data and find the best alpha value that gives the highest accuracy
-        bestAlpha = -1
-        bestAccuracy = -1
-        # laplace smoothing equation 
-        for alpha in alpha_vals:
-            tempProb = {}
-            for (features, labels) in count.items():
-                tempProb[features] = {}
-                for (label, vals) in labels.items():
-                    tempProb[features][label] = {}
-                    total = sum(count[features][label].values())
-                    total += 2*alpha
-                    for (val, c) in vals.items():
-                        #Normalizing the probability
-                        tempProb[features][label][val] = (count[features][label][val] + alpha) / total
-
-            self.probs = tempProb
-
-            predictions = self.classify(validationData)
-
-            # Count number of correct predictions
-            accuracy = 0
-            for i in range(len(predictions)):
-                if predictions[i] == validationLabels[i]:
-                    accuracy += 1
-
-            # Checking if any of the alpha values produced the best accuracy and if it did we store it
-            if accuracy > bestAccuracy:
-                bestAlpha = alpha
-                bestAccuracy = accuracy       
-
-        #Calculating the probabilities using the best alpha to get the most accurate results
-        tProb = {}
-        for (features, labels) in count.items():
-            tProb[features] = {}
-
-            for (label, vals) in labels.items():
-                tProb[features][label] = {}
-                total = sum(count[features][label].values())
-                total += 2*bestAlpha
-                for (val, c) in vals.items():
-                    tProb[features][label][val] = (count[features][label][val] + bestAlpha) / total
-
-        self.probs = tProb
+    
+    def train(self, trainingData, trainingLabels):
+        self.classifier.fit(trainingData, trainingLabels)
 
     def classify(self, testData):
-
         guesses = []
-        self.posteriors = [] 
         for datum in testData:
-            posterior = self.calculateLogJointProbabilities(datum)
-            guesses.append(posterior.argMax())
-            self.posteriors.append(posterior)
+            guesses.append(self.classifier.predict([datum]))
         return guesses
-
-    def calculateLogJointProbabilities(self, datum):
-        logJoint = util.Counter()
-
-        for label in self.legalLabels:
-            logJoint[label] = math.log(self.prior[label])
-
-            for (features, val) in datum.items():
-                p = self.probs[features][label][val];
-                logJoint[label] += math.log(p)
-
-        return logJoint       
